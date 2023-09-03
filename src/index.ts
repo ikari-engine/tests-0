dff9b48eeb60e19e073212203be12628a08ea34b
lengors <24527258+lengors@users.noreply.github.com>
2023-09-03 13:33:13 +0100

    feat(documentation): initialize and setup documentation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+/**
+ * Add two numbers together
+ * 
+ * @param a - The first number
+ * @param b - The second number
+ * @returns The sum of the two numbers
+ */
 export function add(a: number, b: number): number {
   return a + b;
 }
-
-console.log("Hello World!");
