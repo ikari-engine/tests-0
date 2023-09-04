e596a36e174f86868718b9dd94d743d6fb921653
lengors <24527258+lengors@users.noreply.github.com>
2023-09-05 00:11:12 +0100

    feat(force-push): attempt force push

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,14 @@
 export function add(a: number, b: number): number {
   return a + b;
 }
+
+/**
+ * Sub two numbers together
+ * 
+ * @param a - The first number
+ * @param b - The second number
+ * @returns The subtraction of the two numbers
+ */
+export function sub(a: number, b: number): number {
+  return a - b;
+}
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,18 @@
 import { assert } from "chai";
-import { add } from "../src";
+import { add, sub } from "../src";
 
-describe("Test suite", () => {
-  it("test", () => {
+describe("global", () => {
+  it("#add", () => {
     const a = 1;
     const b = 2;
     const expected = a + b;
     assert.equal(add(a, b), expected);
+  });
+
+  it("#sub", () => {
+    const a = 1;
+    const b = 2;
+    const expected = a - b;
+    assert.equal(sub(a, b), expected);
   });
 });
